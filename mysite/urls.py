39bb4d6c1b2a0c74dc7d02dc22bdcccf51cd2500
bob ferrero <bfhike@lubuntu1704.vm>
2017-05-05 17:06:17 -0500

    redirected / to /polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect(reverse('polls:index'))),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
