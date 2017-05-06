da04908568d615092c0a018c58263378810cd7c1
bob ferrero <bfhike@lubuntu1704.vm>
2017-05-06 14:19:13 -0500

    added unit tests for option parser

--- polls/OptionParser.py
+++ polls/OptionParser.py
@@ -1,8 +1,10 @@
-from HTMLParser import HTMLParser
-
+from html.parser import HTMLParser
+from http.client import HTTPSConnection
 
 class OptionParser(HTMLParser):
-    URL_FORMAT = 'https://www.google.com/search?q=pets+images+{0}&num=5&tbm=isch&tbo=u&source=univ&sa=X&bih=648'
+
+    URL_HOST = 'www.google.com'
+    URL_FORMAT = '/search?q=pets+images+{0}&num=5&tbm=isch&tbo=u&source=univ&sa=X&bih=648'
     IMAGE_URL_ATTR_START = "imgurl="
     IMAGE_URL_ATTR_END = "&amp;"
     HREF_ATTR = "href"
@@ -12,18 +14,24 @@ class OptionParser(HTMLParser):
         if tag == "a":
             for attr in attrs:
                 if attr[0] == self.HREF_ATTR:
+                    print(attr[1])
                     start = attr[1].find(self.IMAGE_URL_ATTR_START)
                     if start > -1:
                         end = attr[1].find(self.IMAGE_URL_ATTR_END)
                         self.urls.append(attr[1][start:end])
-                        break
+                        if len(self.urls) == 3:
+                            self.reset()
+                        else:
+                            break
 
-    @staticmethod
-    def search(term):
-        connection = urllib.urlopen(self.URL_FORMAT.format(term))
-        connection.read()
+    def get(self, term):
+        connection = HTTPSConnection(self.URL_HOST)
+        connection.request("GET", self.URL_FORMAT.format(term))
+        response = connection.getresponse()
+        self.feed(str(response.read()))
+        return self.urls
 
 # instantiate the parser and fed it some HTML
 # main = OptionParser()
-# main.search('dog')
+# urls = main.get('dog')
 # main.urls
--- polls/tests.py
+++ polls/tests.py
@@ -9,6 +9,8 @@ from django.utils import timezone
 # Create your tests here.
 
 from .models import Question
+from .OptionParser import OptionParser
+
 
 def create_question(question_text, days):
     """
@@ -31,24 +33,23 @@ class QuestionMethodTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
  
-
     def test_was_published_recently_with_old_question(self):
-	    """
-	    was_published_recently() should return False for questions whose
-	    pub_date is older than 1 day.
-	    """
-	    time = timezone.now() - datetime.timedelta(days=30)
-	    old_question = Question(pub_date=time)
-	    self.assertIs(old_question.was_published_recently(), False)
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-	    """
-	    was_published_recently() should return True for questions whose
-	    pub_date is within the last day.
-	    """
-	    time = timezone.now() - datetime.timedelta(hours=1)
-	    recent_question = Question(pub_date=time)
-	    self.assertIs(recent_question.was_published_recently(), True)
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
 
 
 class QuestionViewTests(TestCase):
@@ -131,5 +132,15 @@ class QuestionIndexDetailTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+
+class OptionsParserGetTests(TestCase):
+
+    def test_get_options(self):
+        """
+        The get method should get 3 urls
+        """
+        options = OptionParser().get('dog')
+        self.assertEqual(len(options.urls), 3)
 
 
